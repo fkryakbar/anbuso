20236aa7744bea80b00c869f780265b735948d09
fkrydev <fikri.afa289@gmail.com>
2024-06-24 05:19:50 +0800

    remove inFocus

--- resources/js/Components/Account.tsx
+++ resources/js/Components/Account.tsx
@@ -46,7 +46,6 @@ export default function Account({ user }: { user: User }) {
                             value={data.name}
                             className="mt-1 block w-full"
                             autoComplete="name"
-                            isFocused={true}
                             onChange={(e) => setData('name', e.target.value)}
                             required
                         />
